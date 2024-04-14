545e2991e976827f4b3190c9e65c4343f8a93929
Jieun Lee <najee0610@naver.com>
2024-04-14 17:40:25 +0900

    [feat] nav 클릭할 때 해당하는 카테고리를 필터링한 후 렌더링

--- week1/home.js
+++ week1/home.js
@@ -1,40 +1,101 @@
 import { WishList } from "./js/itemList.js";
 
-// 카테고리 상품리스트
-document.addEventListener("DOMContentLoaded", function () {
-  const category_articles = document.querySelectorAll(".category_article");
-
-  WishList.forEach((list, index) => {
-    const category_article = category_articles[index + 1];
-    list.forEach((item) => {
-      const categoryContainer = document.createElement("div");
-      categoryContainer.classList.add("categoryContainer");
-
-      const imgElement = document.createElement("img");
-      imgElement.src = item.imgSrc;
-      imgElement.alt = item.title;
-
-      const titleElement = document.createElement("h4");
-      titleElement.textContent = item.title;
-
-      const likebtnElement = document.createElement("button");
-      likebtnElement.textContent = "❤️";
-
-      const priceElement = document.createElement("price");
-      priceElement.textContent = item.price + "won";
-
-      categoryContainer.appendChild(imgElement);
-      categoryContainer.appendChild(titleElement);
-      categoryContainer.appendChild(likebtnElement);
-      categoryContainer.appendChild(priceElement);
-      category_article.appendChild(categoryContainer);
-    });
-  });
-});
-
 // 로고 클릭하면 홈화면 이동
 const logoBtn = document.querySelector(".header_logo-btn");
 
 logoBtn.addEventListener("click", () => {
   window.location.href = "/week1/home.html";
+});
+
+// 상수로 카테고리 아이템 불러오기
+document.addEventListener("DOMContentLoaded", function () {
+  const category_articles = document.querySelector(".category_article");
+
+  WishList.forEach((item) => {
+    const categoryContainer = document.createElement("div");
+    categoryContainer.classList.add("categoryContainer");
+
+    const imgElement = document.createElement("img");
+    imgElement.src = item.imgSrc;
+    imgElement.alt = item.title;
+
+    const titleElement = document.createElement("h4");
+    titleElement.textContent = item.title;
+
+    const likebtnElement = document.createElement("button");
+    likebtnElement.textContent = "❤️";
+
+    const priceElement = document.createElement("price");
+    priceElement.textContent = item.price + "won";
+
+    categoryContainer.appendChild(imgElement);
+    categoryContainer.appendChild(titleElement);
+    categoryContainer.appendChild(likebtnElement);
+    categoryContainer.appendChild(priceElement);
+    category_articles.appendChild(categoryContainer);
+  });
+});
+
+// 카테고리 filter기능
+document.addEventListener("DOMContentLoaded", function () {
+  const categoryNav = document.querySelector(".category_nav ol");
+  const categorySection = document.querySelector(".category_section article");
+
+  // nav 클릭시
+  categoryNav.addEventListener("click", function (event) {
+    if (event.target.tagName === "LI") {
+      const category = event.target.textContent.trim();
+      if (category == "디저트") {
+        filterItems("dessert");
+      } else if (category == "상품권") {
+        filterItems("paper");
+      } else if (category == "생필품") {
+        filterItems("routine");
+      } else {
+        filterItems("전체");
+      }
+    }
+  });
+
+  function filterItems(category) {
+    categorySection.innerHTML = "";
+
+    if (category === "전체") {
+      WishList.forEach((item) => {
+        renderCategoryItem(item);
+      });
+    } else {
+      const filteredItems = WishList.filter(
+        (item) => item.category === category,
+      );
+      filteredItems.forEach((item) => {
+        renderCategoryItem(item);
+      });
+    }
+  }
+
+  function renderCategoryItem(item) {
+    const categoryContainer = document.createElement("div");
+    categoryContainer.classList.add("categoryContainer");
+
+    const imgElement = document.createElement("img");
+    imgElement.src = item.imgSrc;
+    imgElement.alt = item.title;
+
+    const titleElement = document.createElement("h4");
+    titleElement.textContent = item.title;
+
+    const likebtnElement = document.createElement("button");
+    likebtnElement.textContent = "❤️";
+
+    const priceElement = document.createElement("span");
+    priceElement.textContent = item.price + "원";
+
+    categoryContainer.appendChild(imgElement);
+    categoryContainer.appendChild(titleElement);
+    categoryContainer.appendChild(likebtnElement);
+    categoryContainer.appendChild(priceElement);
+
+    categorySection.appendChild(categoryContainer);
+  }
 });
