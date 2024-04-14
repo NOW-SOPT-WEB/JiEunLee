9272d777e54787ed9b93f8723600ba5b4c8903e7
Jieun Lee <najee0610@naver.com>
2024-04-14 15:41:28 +0900

    [feat] 상수 데이터로 상품 카테고리 나타내도록 수정

--- week1/home.js
+++ week1/home.js
@@ -1,3 +1,38 @@
+import { WishList } from "./js/itemList.js";
+
+// 카테고리 상품리스트
+document.addEventListener("DOMContentLoaded", function () {
+  const category_articles = document.querySelectorAll(".category_article");
+
+  WishList.forEach((list, index) => {
+    const category_article = category_articles[index + 1];
+    list.forEach((item) => {
+      const categoryContainer = document.createElement("div");
+      categoryContainer.classList.add("categoryContainer");
+
+      const imgElement = document.createElement("img");
+      imgElement.src = item.imgSrc;
+      imgElement.alt = item.title;
+
+      const titleElement = document.createElement("h4");
+      titleElement.textContent = item.title;
+
+      const likebtnElement = document.createElement("button");
+      likebtnElement.textContent = "❤️";
+
+      const priceElement = document.createElement("price");
+      priceElement.textContent = item.price + "won";
+
+      categoryContainer.appendChild(imgElement);
+      categoryContainer.appendChild(titleElement);
+      categoryContainer.appendChild(likebtnElement);
+      categoryContainer.appendChild(priceElement);
+      category_article.appendChild(categoryContainer);
+    });
+  });
+});
+
+// 로고 클릭하면 홈화면 이동
 const logoBtn = document.querySelector(".header_logo-btn");
 
 logoBtn.addEventListener("click", () => {
