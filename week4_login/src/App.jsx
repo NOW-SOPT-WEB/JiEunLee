cc2342dfe946f021a12b49ec496f4963dd805985
Jieun Lee <najee0610@naver.com>
2024-05-08 21:21:27 +0900

    chore: 프로젝트 초기 설정

---
+++ week4_login/src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css';
+import * as React from 'react';
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ week4_login/src/styles/theme.js
@@ -0,0 +1,14 @@
+const colors = {
+  white: '#FFFFFF',
+  black: '#000000',
+  lightPink: '#FCC8F8',
+  lightPurple: '#EAB4F8',
+  purple: '#9900F0',
+  lightYellow: '#F3F798',
+};
+const theme = {
+  colors,
+  fonts,
+};
+
+export default theme;
