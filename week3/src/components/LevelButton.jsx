b9a7786a288c17f1f6ef858949eea0810029784f
Jieun Lee <najee0610@naver.com>
2024-06-13 11:22:26 +0900

    feat: 난이도 중간에 바꿀시/게임 도중 reset버튼 클릭시 카드 처음으로 변경

--- week3/src/components/LevelButton.jsx
+++ week3/src/components/LevelButton.jsx
@@ -1,35 +1,29 @@
 /* eslint-disable react/prop-types */
 import styled from "styled-components";
 
-function LevelButton({
-  setCards,
-  setDifficulty,
-  difficulty,
-  getMixedCardList,
-}) {
+function LevelButton({ setCards, setDifficulty, difficulty, getMixedCardList, setScore }) {
   const handleChangeLevel = (difficulty, count) => {
-    setCards(getMixedCardList(count));
+    // 모든 카드를 뒤집고 점수 초기화
+    setCards((prevCards) => prevCards.map((card) => ({ ...card, status: false })));
+
+    setTimeout(() => {
+      setCards(getMixedCardList(count));
+      setScore(0); // 점수 초기화
+    }, 1000);
+
     setDifficulty(difficulty);
   };
+
   return (
     <>
       <ButtonWrapper>
-        <Button
-          onClick={() => handleChangeLevel("easy", 5)}
-          $isSelected={"easy" === difficulty}
-        >
+        <Button onClick={() => handleChangeLevel("easy", 5)} $isSelected={"easy" === difficulty}>
           Easy
         </Button>
-        <Button
-          onClick={() => handleChangeLevel("normal", 7)}
-          $isSelected={"normal" === difficulty}
-        >
+        <Button onClick={() => handleChangeLevel("normal", 7)} $isSelected={"normal" === difficulty}>
           Normal
         </Button>
-        <Button
-          onClick={() => handleChangeLevel("hard", 9)}
-          $isSelected={"hard" === difficulty}
-        >
+        <Button onClick={() => handleChangeLevel("hard", 9)} $isSelected={"hard" === difficulty}>
           Hard
         </Button>
       </ButtonWrapper>
@@ -50,8 +44,7 @@ const Button = styled.button`
   width: 10vw;
   height: 4vh;
   background-color: ${({ theme }) => theme.colors.lightYellow};
-  background-color: ${({ $isSelected, theme }) =>
-    $isSelected ? theme.colors.lightPink : theme.colors.lightYellow};
+  background-color: ${({ $isSelected, theme }) => ($isSelected ? theme.colors.lightPink : theme.colors.lightYellow)};
   border: none;
   border-radius: 30px;
   &:hover {
